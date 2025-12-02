75809a98d9bfc680c3043fc90556e655218f6c2a
Gizem Merve Demir <gizemervedemir@gmail.com>
2025-12-02 06:19:41 +0300

    [Data] Implement ServiceRepository and ServiceRepositoryImpl issue#12

---
+++ app/src/main/java/com/example/studentcenterapp/data/service/Service.kt
@@ -0,0 +1,7 @@
+package com.example.studentcenterapp.data.service
+// TODO: implemented by gizem merve demir
+data class Service (
+    val id: String = "",
+    val departmentId: String = "",
+    val name: String = ""
+)
---
+++ app/src/main/java/com/example/studentcenterapp/data/service/ServiceRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package com.example.studentcenterapp.data.service
+
+import kotlinx.coroutines.flow.Flow
+
+class ServiceRepositoryImpl(
+    private val dataSource: ServiceDataSource
+) : ServiceRepository {
+
+    override fun getServicesByDepartment(departmentId: String): Flow<List<Service>> {
+        return dataSource.getServicesByDepartment(departmentId)
+    }
+}
